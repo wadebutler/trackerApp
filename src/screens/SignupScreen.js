7dc4b3c31e47f14e2f1c531ddd5ba4738c9f1d83
Wade Butler <wadebutler2@gmail.com>
2020-07-16 16:35:01 -0400

    added context

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../Components/Spacer';
 
 const SignupScreen = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <View style={styles.container}>
             <Spacer>
                 <Text h3>Sign Up for Tracker</Text>
             </Spacer>
             <Spacer>
-                <Input label="Email" />
+                <Input 
+                    label="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
             </Spacer>
             <Spacer>
-                <Input label="Password" />
+                <Input 
+                    label="Password" 
+                    value={password}
+                    onChangeText={setPassword}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry
+                />
             </Spacer>
             <Spacer>
                 <Button title="Sign Up"/>
