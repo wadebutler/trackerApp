d8e9e5c602dc1f962d80f18ea726070f00ac2354
Wade Butler <wadebutler2@gmail.com>
2020-07-21 20:32:08 -0400

    built mock movement code

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import '../_mockLocation';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet } from 'react-native';
 import {Text} from 'react-native-elements';
 import {SafeAreaView} from 'react-navigation';
+import { requestPermissionsAsync } from 'expo-location';
 import Map from '../Components/Map';
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            const { granted } = await requestPermissionsAsync();
+            if (!granted) {
+                throw new Error('Location permission not granted');
+            }
+        } catch (e) {
+            setErr(e);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, [])
+
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
             <Text style={style.header} h3>Track Create Screen</Text>
             <Map />
+            {err ? <Text>Please enable location services</Text> : null}
         </SafeAreaView>
     );
 }
