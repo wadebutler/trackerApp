38a03b9d47a490d8d6ab2f1e66c845aae8179321
Wade Butler <wadebutler2@gmail.com>
2020-07-27 20:09:42 -0400

    error handling and bug fixing

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,35 +3,44 @@ import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-loca
 
 export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null);
-    const [subscriber, setSubscriber] = useState(null)
-
-    const startWatching = async () => {
-        try {
-            const { granted } = await requestPermissionsAsync();
-            const sub = await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10,
-            },
-                callback
-            )
-            if (!granted) {
-                throw new Error('Location permission not granted');
-            }
-            setSubscriber(sub)
-        } catch (e) {
-            setErr(e);
-        }
-    };
+    // const [subscriber, setSubscriber] = useState(null)
 
     useEffect(() => {
+        let subscriber;
+
+        const startWatching = async () => {
+            try {
+                const { granted } = await requestPermissionsAsync();
+                subscriber = await watchPositionAsync({
+                    accuracy: Accuracy.BestForNavigation,
+                    timeInterval: 1000,
+                    distanceInterval: 10,
+                },
+                    callback
+                )
+                if (!granted) {
+                    throw new Error('Location permission not granted');
+                }
+            } catch (e) {
+                setErr(e);
+            }
+        };
+
         if (shouldTrack) {
             startWatching();
         } else {
-            subscriber.remove();
-            setSubscriber(null);
+            if (subscriber) {
+                subscriber.remove();
+            }
+            subscriber = null;
         }
-    }, [shouldTrack])
+
+        return () => {
+            if (subscriber) {
+                subscriber.remove();
+            }
+        }
+    }, [shouldTrack, callback])
 
     return [err];
 }
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,5 +1,5 @@
 import '../_mockLocation';
-import React, {useContext} from 'react';
+import React, {useContext, useCallback} from 'react';
 import { StyleSheet } from 'react-native';
 import {Text} from 'react-native-elements';
 import {SafeAreaView, withNavigationFocus} from 'react-navigation';
@@ -9,10 +9,11 @@ import useLocation from '../hooks/useLocation';
 import TrackForm from '../Components/TrackForm';
 
 const TrackCreateScreen = ({isFocused}) => {
-    const {state, addLocation} = useContext(LocationContext)
-    const [err] = useLocation(isFocused, (location) => {
-        addLocation(location, state.recording)
-    })
+    const {state: {recording}, addLocation} = useContext(LocationContext)
+    const callback = useCallback((location) => {
+        addLocation(location, recording)
+    }, [recording])
+    const [err] = useLocation(isFocused || recording, callback)
 
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
